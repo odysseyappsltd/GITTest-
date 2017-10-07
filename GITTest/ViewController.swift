8f49160b33e31f880be26055a6fe637a562218b0
Admin <admin@macbook-pro-4.local>
2017-10-07 13:45:45 +0600

    B World

--- GITTest/ViewController.swift
+++ GITTest/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //Hello World
+        //B World
     }
 
     override func didReceiveMemoryWarning() {
